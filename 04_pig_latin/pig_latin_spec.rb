e3166f7651c152bcb2117a864f14c335463abf6c
Gary Huong <garyhuong@gmail.com>
2017-05-20 09:51:51 -0400

    Completed 04 pig latin

--- 04_pig_latin/pig_latin_spec.rb
+++ 04_pig_latin/pig_latin_spec.rb
@@ -65,6 +65,11 @@ describe "#translate" do
     expect(s).to eq("ethay ickquay ownbray oxfay")
   end
 
+  it 'translates a word containing only consonants' do
+    s = translate("kqu")
+    expect(s).to eq("kquay")
+  end
+
   # Test-driving bonus:
   # * write a test asserting that capitalized words are still capitalized (but with a different initial capital letter, of course)
   # * retain the punctuation from the original phrase
